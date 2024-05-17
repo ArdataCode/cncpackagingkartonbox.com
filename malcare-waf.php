c5af79daac2b576f57e29ffa19ec08dfe9e0b7a4
ArdataCode <ardata.indonesia@gmail.com>
2024-05-17 15:29:01 +0700

    mit

---
+++ malcare-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/ujianori/cncpackagingkartonbox.com/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/home/ujianori/cncpackagingkartonbox.com/wp-content/mc_data/');
+	define("MCCONFKEY", 'ad9340d12f0ab894873a4b09735e4ccf');
+	include_once('/home/ujianori/cncpackagingkartonbox.com/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'upK5YfNQI44sFB46hDWIWA2R45VXqY4UED6AI9bT9xpC917SdIaJSIBM89J3EX4c');
+define('SECURE_AUTH_KEY',  'CMMstNsfoCUr2L6q4o4yC7UcKdoHxx02yp1tMC8ow75djBt78yfUASNz0gNPvInE');
+define('LOGGED_IN_KEY',    'XSDnz1A1czWvuGBBJXA3nFDCWYK1Qsa99JxIgiD93DnGag5RBKPr411qCQpVoKm1');
+define('NONCE_KEY',        'hgUSCDQXuLWg183EE2TbXSXXQhcPTUmqce4YIsbwMg8f5h2CXAEvV5VtEcGFSUNz');
+define('AUTH_SALT',        'FqIYSmMf3EEXwscxUMCn02IJLXap4se313MYzd8jMacmePSXmLozaS2YrgbqnHYN');
+define('SECURE_AUTH_SALT', 'yEazRLLAN37yvFssypeAeApCGJmme9fMGXpt54XGh5xapIzjc3FYww1hGdEuU9fA');
+define('LOGGED_IN_SALT',   'HKPBroLmItb8vpIDtvDHnfegIcHPvzxzzsjL8ofw45hzB4fPyyysQdHMRgBpy9BJ');
+define('NONCE_SALT',       'hAP7XN75mnQ6pwe2c2dXfDW1NC76PUTcI458eXXVYEhNdz3V0ARiU1IBVUoHUjG3');
